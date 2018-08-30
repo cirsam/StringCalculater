a470a8cde7e92c9406a77d482d1d853a5d1d2671
Samuel Antwi <cirsam@hotmail.com>
2018-08-30 06:21:30 -0400

    Add the calculator object and the unit test

---
+++ StringCalculatorTests/StringCalculatorObjectTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestClass()]
+    public class StringCalculatorObjectTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+
+    }
+}
